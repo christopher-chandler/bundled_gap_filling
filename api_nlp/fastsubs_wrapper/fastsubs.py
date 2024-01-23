76dac79726a76ccc90d14224ff1aaee146852d22
Christopher Michael Chandler <christopher.chandler@outlook.de>
2024-01-23 13:00:34 +0100

    add fastsubs

--- api_nlp/fastsubs_wrapper/fastsubs.py
+++ api_nlp/fastsubs_wrapper/fastsubs.py
@@ -1,30 +1,25 @@
-# Standard
 import subprocess
 
-# Pip
-# None
-
-# Custom
-# None
-
-
 class FastSubs:
-    def __init__(self):
-        self.n_gram = 2
-        self.model = "../../data/language_model/en-70k-0.2-pruned.lm"
-        self.incoming_data = " ../data/incoming_text_data/sentences.txt"
+    def __init__(self, n_gram=2,
+                 model="../../data/language_model/en-70k-0.2-pruned.lm",
+                 incoming_data="../data/incoming_text_data/sentences.txt",
+                 fastsubs_m1="/Users/christopherchandler/code_repos/christopher-chandler"
+                             "/Python/nlp/rub/bundled_gap_filling/api_nlp/fastsubs_wrapper/fastsubs_m1"):
+        self.n_gram = n_gram
+        self.model = model
+        self.incoming_data = incoming_data
+        self.fastsubs_m1 = fastsubs_m1
 
     def run_fastsubs(self, input_sentences):
         command = [
-            "/Users/christopherchandler/code_repos/christopher-chandler/Python/nlp/"
-            "rub/bundled_gap_filling/api_nlp/fastsubs_wrapper/fastsubs_m1",
+            self.fastsubs_m1,
             "-n",
             str(self.n_gram),
             self.model,
             self.incoming_data,
         ]
 
-        # Open subprocess with stdin as a pipe and redirect stdout and stderr to pipes
         with subprocess.Popen(
             command,
             stdin=subprocess.PIPE,
@@ -32,13 +27,9 @@ class FastSubs:
             stderr=subprocess.PIPE,
             text=True,
         ) as process:
-            # Pass input sentences to the subprocess
-            process_input = "\n".join(['Sarah eats apples too'])
-
-            # Communicate with the subprocess
+            process_input = "\n".join(input_sentences)
             process_output, _ = process.communicate(input=process_input)
 
-            # Filter out and print lines that don't contain the initial message
             filtered_output = "\n".join(
                 line
                 for line in process_output.splitlines()
@@ -46,12 +37,10 @@ class FastSubs:
             )
             results = filtered_output.split("\n")
 
-            print(input_sentences)
             prob_replace = dict()
 
             for row in results:
                 row = row.split("\t")
-
                 word, replacements = row[0], row[1:]
                 prob_replace[word] = replacements
 
@@ -61,7 +50,6 @@ class FastSubs:
 if __name__ == "__main__":
     fastsubs_instance = FastSubs()
 
-    # Example input sentences
     input_sentences = ["Sarah eats apples too"]
     prob = fastsubs_instance.run_fastsubs(input_sentences)
     print(prob)
--- main.py
+++ main.py
@@ -42,6 +42,19 @@ def generate_bundle(incoming_data:str, target_word = "" ) -> None:
 if __name__ == "__main__":
     incoming_data = "data/incoming_text_data/sentences.txt"
     generate_bundle(incoming_data, target_word="apple")
-    fastsubs_instance = FastSubs()
+
+    from api_nlp.fastsubs_wrapper.fastsubs import FastSubs
+
+    fastsubs_instance = FastSubs(
+        model="/Users/christopherchandler/code_repos/christopher-chandler/Python/nlp/rub/bundled_gap_filling/data/language_model/en-70k-0.2-pruned.lm",
+        incoming_data="/Users/christopherchandler/code_repos/christopher-chandler/Python/nlp/rub/bundled_gap_filling/data/incoming_text_data/sentences.txt",
+        fastsubs_m1="/Users/christopherchandler/code_repos/christopher-chandler/Python/nlp/rub/bundled_gap_filling/api_nlp/fastsubs_wrapper/fastsubs_m1",
+        n_gram=2
+    )
+
+    # Example input sentences
+    input_sentences = ["Sarah eats apples too"]
+    prob = fastsubs_instance.run_fastsubs(input_sentences)
+    print(prob)
 
 
